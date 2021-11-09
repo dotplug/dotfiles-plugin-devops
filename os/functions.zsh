2e7bb1facdcb4fcdd48a39f380f573bf506c989a
Juanan <juananortizc@autentia.com>
2021-11-09 16:33:32 +0100

    Delete os non-required files that are present in the common dotfiles

--- os/functions.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Summary: Search for all Brewfile files and creates the merged file inside ~/.Brefile
-function create_brewfiles() {
-    merge_files "Brewfile" "$DOTFILES_DIR" "$HOME/.Brewfile"
-}
