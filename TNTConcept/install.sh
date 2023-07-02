a67e875cb44bee723b0da3f5fd89eeb55499c30a
Juanan <jaocarvajal.c@gmail.com>
2023-07-02 20:04:55 +0200

    fix: updated tnt install script don't une any lib helpers

--- TNTConcept/install.sh
+++ TNTConcept/install.sh
@@ -6,12 +6,7 @@ IFS=$'\n\t'
 DOTFILES_TNT_USERNAME=${DOTFILES_TNT_USERNAME:-}
 DOTFILES_TNT_PASSWORD=${DOTFILES_TNT_PASSWORD:-}
 
-# Needs to load the helpers before using it
-source "${DOTFILES_ROOT}/lib/helpers"
-
 setup_netrc () {
-    set_debug_file_label "$BASH_SOURCE"
-
     local netrc_filepath=$HOME/.netrc
     local netrc_path_template=$DOTFILES_ROOT/plugins/dotfiles-plugin-devops/TNTConcept/netrc.template
 
@@ -34,19 +29,21 @@ setup_netrc () {
           tnt_password=$DOTFILES_TNT_PASSWORD
         fi
 
-        debug_msg "[TNT Concept] Replace values from the template to the original netrc"
+        blue "[TNT Concept] Replace values from the template to the original netrc"
         sed -e "s/USERNAME/$tnt_username/g" -e "s/PASSWORD/$tnt_password/g" $netrc_path_template > $netrc_filepath
 
-        success_msg '[TNT Concept] netrc applied'
+        green '[TNT Concept] netrc applied'
     else
-        info_msg '[TNT Concept] netrc already exists'
+        blue '[TNT Concept] netrc already exists'
     fi
 }
 
 
 setup_pipconf () {
     local pipconf_path_template=$DOTFILES_ROOT/plugins/dotfiles-plugin-devops/TNTConcept/pip.conf.template
+    blue "[TNT Concept] Ensure pip configuration folder is created"
     mkdir -p $HOME/.config/pip
+    blue "Copy pip configuration"
     cp $pipconf_path_template $HOME/.config/pip/pip.conf
 }
 
@@ -62,14 +59,9 @@ pip3 --version
 blue "[TNT Concept] Show PIP configuration is properly set"
 python3 -m pip config list
 
-blue "[TNT Concept] Ensure that TNTGitHook is installed..."
-if [[ $(pip3 list |grep 'TNTGitHooks') -eq 0 ]]; then
-  blue "[TNT Concept] Installed, no action required"
-else
-  blue "[TNT Concept] Install TNTGitHook in the system"
-  python3 -m pip install --upgrade TNTGitHook --user
+blue "[TNT Concept] Install TNTGitHook in the system"
+python3 -m pip install --upgrade TNTGitHook --user
 
-  blue "[TNT Concept] Setup credentials"
-  python3 -m TNTGitHook --set-credentials
-fi
+blue "[TNT Concept] Setup credentials"
+python3 -m TNTGitHook --set-credentials
 
