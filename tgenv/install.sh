937524ad91f8b27abc5f2065efdf98fe19b025d5
Juanan <jaocarvajal.c@gmail.com>
2024-09-15 23:58:01 +0200

    feat: changed the tgenv repository to the fork that is used

--- tgenv/install.sh
+++ tgenv/install.sh
@@ -4,8 +4,8 @@ set -uo pipefail
 IFS=$'\n\t'
 
 if [[ ! -d ~/.tgenv ]]; then
-    # Installation process followed by https://github.com/cunymatthieu/tgenv
+    # Installation process followed by https://github.com/tgenv/tgenv
     blue "Installing tgenv"
-    git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv
+    git clone https://github.com/tgenv/tgenv.git ~/.tgenv
     green "Successfully installed tgenv"
 fi
